97eed8ea43280d820fae5b2f908822b76a72b3e4
Kelsey Dickson <kelseymedved@gmail.com>
2020-05-05 17:38:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,13 @@
 #require_relative './spec_helper.rb'
 #require_relative '../fizzbuzz_spec.rb'
 def fizzbuzz(int)
-  if int % 3 == 0
-    return 'Fizz'
+  if int % 3 == 0 and int % 5 == 0
+    return "FizzBuzz"
   elsif int % 5 == 0
-    return 'Buzz'
-  else int % 5 and 3 == 0
-    return 'FizzBuzz'
-end
+    return "Buzz"
+  elsif int % 3 == 0
+    return  "Fizz"
+
+  end
+
 end
